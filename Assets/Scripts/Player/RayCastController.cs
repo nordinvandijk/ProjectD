432c7a7f3fbf1b852ac16742de8d6ed80f245978
Saronny <mikebelike1213@gmail.com>
2023-03-28 02:50:25 +0200

    Added raycast info

--- Assets/Scripts/Player/RayCastController.cs
+++ Assets/Scripts/Player/RayCastController.cs
@@ -5,23 +5,46 @@ using UnityEngine;
 public class RayCastController : MonoBehaviour
 {
   
+    public GameObject UI;
+
+    void Start()
+    {
+        UI.SetActive(false);
+    }
+
     void Update()
     {
         RaycastHit hit;
         // Does the ray intersect any objects excluding the player layer
 
-        //Raycast from the center of the camera
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hit, Mathf.Infinity))
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            //Change the color of the object that is hit
-            hit.transform.gameObject.GetComponent<Renderer>().material.color = Color.red;
-            Debug.Log("Did Hit");
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.back) * hit.distance, Color.green);
+            //Change the color of the object that is hit and clicked
+            if (Input.GetMouseButtonDown(0) && hit.transform.gameObject.tag == "Interactable")
+            {
+                hit.transform.gameObject.GetComponent<Renderer>().material.color = Color.red;
+                //log the coordinates of the object that is hit and display textbox by the object that is hit and clicked for the user to read the information of the object
+                Debug.Log("Hit at " + hit.point);
+                UI.SetActive(true);
+                UI.transform.position = hit.transform.position;
+            }
+            
         }
         else
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.back) * 1000, Color.white);
             Debug.Log("Did not Hit");
+            UI.SetActive(false);
+            
         }
     }
+ 
+    //draw the ray in the scene view for debugging purposes backwords
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.TransformDirection(Vector3.back) * 1000);
+    }
+    
 }
